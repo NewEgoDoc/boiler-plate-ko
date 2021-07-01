a5781fc5c66732c7a8673e7f1b37b51d1a098442
unknown <juhan211@naver.com>
2021-07-01 13:25:23 +0900

    post맵핑으로 회원가입 기본 백단 틀 만들기

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const { User } = require('./models/User');
+
+
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.json())
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://mongoUser:asdf123123@cluster0.ptsdh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
     useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false
@@ -10,6 +17,19 @@ mongoose.connect('mongodb+srv://mongoUser:asdf123123@cluster0.ptsdh.mongodb.net/
 
 app.get('/',(req,res)=>{
     res.send('Hellow World');
+})
+
+app.post('/register',(req,res)=>{
+
+
+    const user = new User(req.body)
+
+    user.save((err,userInfo)=>{
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
 })
 
 app.listen(port,()=>console.log(`Example app listening on port ${port}`))
